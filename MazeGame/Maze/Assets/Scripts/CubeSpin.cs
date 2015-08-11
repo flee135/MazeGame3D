cabbe8f3947a23fb88b2c9beb6d7b2814c499683
Felix Lee <felix.lee135@gmail.com>
2015-08-11 02:28:17 -0700

    Fixed bug with wall scaling. Began coroutine for hovering cube but I need sleep.

--- MazeGame/Maze/Assets/Scripts/CubeSpin.cs
+++ MazeGame/Maze/Assets/Scripts/CubeSpin.cs
@@ -3,10 +3,35 @@ using System.Collections;
 
 public class CubeSpin : MonoBehaviour {
 
-	public float speed;
+	public float spinSpeed;
+	public float floatingSpeed;
+	public float floatingDistance;
+
+	void Awake() {
+		//StartCoroutine ("LoopFloating");
+	}
+
+	IEnumerator LoopFloating() {
+		float dir = 1f;
+		print (1);
+		while (true) {
+			print (2);
+			while (Mathf.Abs (transform.position.y - 1.5f) < floatingDistance) {
+				print (3);
+				float dy = Time.deltaTime * floatingSpeed;
+				transform.position = new Vector3(transform.position.x, transform.position.y+dy*dir, transform.position.z);
+				print (4);
+				yield return null;
+			}
+			print (5);
+			dir *= -1;
+		}
+	}
 
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (Vector3.up, speed*10*Time.deltaTime, Space.World);
+		transform.Rotate (Vector3.up, spinSpeed*10*Time.deltaTime, Space.World);
 	}
+
+
 }
